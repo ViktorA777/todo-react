59cb8c7dcb84ad3d197ed277fd8b40cd94e73786
Viktor <vitya.avilov777@mail.ru>
2023-04-29 15:48:28 +0300

    Закончил верстку

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,8 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import Input from "../input/Input";
+import Item from "../item/Item";
+import union from "../../assets/icons/union.svg";
 
 import styles from "../../assets/styles/style.module.scss";
 
@@ -12,9 +14,20 @@ function App() {
         <Input text="Поиск" />
         <div className={styles.content}>
           <Input text="Введите текст" />
-          <button className={styles.button}>Добавить +</button>
+          <button className={styles.button}>
+            Добавить <img className={styles.union} src={union} />
+          </button>
         </div>
       </div>
+      <Item text="Выбрать хостинг для сайта" />
+      <Item text="Записаться к стоматологу" />
+      <Item text="Записаться на курсы по Английскому" />
+      <Item text="Навести порядок на кухне" />
+      <Item text="Подготовить одежду к поездке" />
+      <Item text="Подготовиться к выступлению в понедельник" />
+      <Item text="Помыть машину" />
+      <Item text="Купить продукты" />
+      <Item text="Отдать проект на проверку" />
       <Footer />
     </div>
   );
