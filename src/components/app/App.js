7d01bc200fe2387a01b86128efc381f35f1a8182
Viktor <vitya.avilov777@mail.ru>
2023-05-01 16:42:14 +0300

    добавил функционал по удалению todo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import Input from "../input/Input";
-import Item from "../item/Item";
+import ItemList from "../itemList/ItemList";
 import union from "../../assets/icons/union.svg";
 
 import styles from "../../assets/styles/style.module.scss";
 
 function App() {
+  const [todo, setTodo] = useState([
+    { id: 1, title: "Выбрать хостинг для сайта" },
+    { id: 2, title: "Записаться к стоматологу" },
+    { id: 3, title: "Записаться на курсы по Английскому" },
+    { id: 4, title: "Навести порядок на кухне" },
+    { id: 5, title: "Подготовить одежду к поездке" },
+    { id: 6, title: "Подготовиться к выступлению в понедельник" },
+    { id: 7, title: "Помыть машину" },
+    { id: 8, title: "Купить продукты" },
+    { id: 9, title: "Купить продукты" },
+  ]);
+
   return (
     <div className={styles.wrapper}>
       <Header />
@@ -19,15 +33,7 @@ function App() {
           </button>
         </div>
       </div>
-      <Item text="Выбрать хостинг для сайта" />
-      <Item text="Записаться к стоматологу" />
-      <Item text="Записаться на курсы по Английскому" />
-      <Item text="Навести порядок на кухне" />
-      <Item text="Подготовить одежду к поездке" />
-      <Item text="Подготовиться к выступлению в понедельник" />
-      <Item text="Помыть машину" />
-      <Item text="Купить продукты" />
-      <Item text="Отдать проект на проверку" />
+      <ItemList todo={todo} setTodo={setTodo} />
       <Footer />
     </div>
   );
--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -2,13 +2,20 @@ import trash from "../../assets/icons/trash.svg";
 import edit from "../../assets/icons/edit.svg";
 import styles from "./item.module.scss";
 
-function Item({ text }) {
+function Item({ title, onDeleteTodo, todo }) {
   return (
     <div className={styles.container}>
-      <span className={styles.text}>{text}</span>
+      <span className={styles.text}>{title}</span>
       <div className={styles.img}>
-        <img src={trash} />
-        <img src={edit} />
+        <button
+          onClick={() => onDeleteTodo(todo.id)}
+          className={styles.background}
+        >
+          <img src={trash} />
+        </button>
+        <button className={styles.background}>
+          <img src={edit} />
+        </button>
       </div>
     </div>
   );
